03140b22f5d0c3d9f211e9722faf3ed431e64e30
Davide Laezza <truzzialrogo@gmx.com>
2021-08-15 13:07:39 +0200

    style: formatting codebase

--- src/main/scala/org/primeservices/PrimesUpTo.scala
+++ src/main/scala/org/primeservices/PrimesUpTo.scala
@@ -2,15 +2,22 @@ package org.primeservices
 
 object PrimesUpTo {
   // Courtesy of Wikipedia: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
-  private def sieveOfEratosthenes(upTo: Int, divisor: Int, maybePrimes: Seq[Int]): Seq[Int] =
+  private def sieveOfEratosthenes(
+      upTo: Int,
+      divisor: Int,
+      maybePrimes: Seq[Int]
+  ): Seq[Int] =
     if (divisor > floorSqrt(upTo)) {
       maybePrimes
-    }
-    else {
-      val filteredPrimes = maybePrimes.filter(n => n == divisor || n % divisor != 0)
-      filteredPrimes.find(_ > divisor).map { newDivisor =>
-        sieveOfEratosthenes(upTo, newDivisor, filteredPrimes)
-      }.getOrElse(filteredPrimes)
+    } else {
+      val filteredPrimes =
+        maybePrimes.filter(n => n == divisor || n % divisor != 0)
+      filteredPrimes
+        .find(_ > divisor)
+        .map { newDivisor =>
+          sieveOfEratosthenes(upTo, newDivisor, filteredPrimes)
+        }
+        .getOrElse(filteredPrimes)
     }
 
   def sieveOfEratosthenes(upTo: Int): Seq[Int] =
--- src/test/scala/org/primeservices/PrimesUpToSpec.scala
+++ src/test/scala/org/primeservices/PrimesUpToSpec.scala
@@ -1,18 +1,16 @@
 package org.primeservices
 
-import org.scalatest.wordspec.AnyWordSpec
-import org.scalatest.matchers.should
-import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 import org.scalacheck.Gen
-
-import scala.language.postfixOps
 import org.scalatest.matchers.BeMatcher
 import org.scalatest.matchers.MatchResult
+import org.scalatest.matchers.should
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
-class SieveOfEratosthenesSpec extends AnyWordSpec
-  with ScalaCheckPropertyChecks
-  with should.Matchers
-{
+class SieveOfEratosthenesSpec
+    extends AnyWordSpec
+    with ScalaCheckPropertyChecks
+    with should.Matchers {
   /*
    * This could be an implicit, since it's used in the majority of tests.
    * However, I find that implicits tend to cause unreadable and spaghetti
@@ -22,12 +20,12 @@ class SieveOfEratosthenesSpec extends AnyWordSpec
 
   // Courtesy of Wikipedia: https://en.wikipedia.org/wiki/Primality_test#Simple_methods
   private val prime = new BeMatcher[Int] {
-      def apply(n: Int) = MatchResult(
-        (2 to floorSqrt(n)).forall (n % _ != 0),
-        s"$n is not prime",
-        s"$n is prime",
-      )
-    }
+    def apply(n: Int) = MatchResult(
+      (2 to floorSqrt(n)).forall(n % _ != 0),
+      s"$n is not prime",
+      s"$n is prime"
+    )
+  }
 
   "sieveOfEratosthenes" should {
     /*
@@ -39,27 +37,27 @@ class SieveOfEratosthenesSpec extends AnyWordSpec
      */
     "return all the prime numbers less than or equal to the upper bound" in {
       val expected = Stream(2, 3, 5, 7, 11, 13, 17, 19, 23, 29)
-      PrimesUpTo.sieveOfEratosthenes(30) should equal (expected)
+      PrimesUpTo.sieveOfEratosthenes(30) should equal(expected)
     }
 
     "only return prime numbers" in {
-      forAll (strictlyPositiveInts) { (n: Int) =>
+      forAll(strictlyPositiveInts) { (n: Int) =>
         val primes = PrimesUpTo.sieveOfEratosthenes(n)
-        every (primes) shouldBe prime
+        every(primes) shouldBe prime
       }
     }
 
     "only return numbers less than or equal to the upper bound" in {
-      forAll (strictlyPositiveInts) { (n: Int) =>
+      forAll(strictlyPositiveInts) { (n: Int) =>
         val primes = PrimesUpTo.sieveOfEratosthenes(n)
-        every (primes) should be <= n
+        every(primes) should be <= n
       }
     }
 
     "only return positive numbers" in {
-      forAll (strictlyPositiveInts) { (n: Int) =>
+      forAll(strictlyPositiveInts) { (n: Int) =>
         val primes = PrimesUpTo.sieveOfEratosthenes(n)
-        every (primes) should be > 0
+        every(primes) should be > 0
       }
     }
   }
