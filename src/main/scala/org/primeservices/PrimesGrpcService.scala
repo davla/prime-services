89400418feeb43f25f058445b81bc5bdd3c65acc
Davide Laezza <truzzialrogo@gmx.com>
2021-08-15 19:30:22 +0200

    feature: add error handling in primes gRPC service

--- src/main/scala/org/primeservices/PrimesGrpcService.scala
+++ src/main/scala/org/primeservices/PrimesGrpcService.scala
@@ -1,17 +1,27 @@
 package org.primeservices
 
 import akka.stream.Materializer
+import io.grpc.Status
+
 import scala.concurrent.Future
+import scala.util.Try
 
 class PrimesGrpcService(private val computer: PrimesComputer)
     extends PrimesService {
 
   override def getPrimesUpTo(in: PrimesRequest): Future[PrimesResponse] =
-    Future.successful(
-      PrimesResponse(
-        computer(in.upTo)
-      )
-    )
+    Try(computer(in.upTo))
+      .map { primes => Future.successful(PrimesResponse(primes)) }
+      .recover(error => {
+        val status = error match {
+          case _ if error.isInstanceOf[IllegalArgumentException] =>
+            Status.INVALID_ARGUMENT
+              .augmentDescription(error.getMessage())
+          case _ => Status.fromThrowable(error)
+        }
+        Future.failed(status.asRuntimeException())
+      })
+      .get
 }
 
 object PrimesGrpcService {
--- src/test/scala/org/primeservices/PrimesGrpcServiceSpec.scala
+++ src/test/scala/org/primeservices/PrimesGrpcServiceSpec.scala
@@ -1,5 +1,7 @@
 package org.primeservices
 
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.BeforeAndAfterEach
 import org.scalatest.concurrent.ScalaFutures
@@ -30,6 +32,42 @@ class PrimesGrpcServiceSpec
 
       // Then
       reply.futureValue should equal(expected)
+    }
+
+    "report INVALID_ARGUMENT on invalid input" in {
+      // Given
+      val input = -10
+
+      val computePrimes = mockFunction[Int, Seq[Int]]
+      computePrimes expects(input) throws new IllegalArgumentException()
+
+      val service = PrimesGrpcService(computePrimes)
+
+      // When
+      val reply = service.getPrimesUpTo(PrimesRequest(input))
+
+      // Then
+      val error = reply.failed.futureValue
+      error shouldBe a[StatusRuntimeException]
+      val errorCode =
+        error.asInstanceOf[StatusRuntimeException].getStatus.getCode
+      errorCode should equal(Status.Code.INVALID_ARGUMENT)
+    }
+
+    "forward any other the error thrown by prime computation" in {
+      // Given
+      val input = -10
+
+      val computePrimes = mockFunction[Int, Seq[Int]]
+      computePrimes expects(input) throws new RuntimeException()
+
+      val service = PrimesGrpcService(computePrimes)
+
+      // When
+      val reply = service.getPrimesUpTo(PrimesRequest(input))
+
+      // Then
+      reply.failed.futureValue shouldBe a[RuntimeException]
     }
   }
 }
